1f07d1ac628fce60c0635fb36ab870c51acdc157
Alberto JR <albertojunquera.ra@gmail.com>
2023-06-19 12:47:05 +0200

    Fixed string format navigation

--- app/src/main/java/com/albertojr/proyectoandroidsuperpoderes/ui/navigation/Screens.kt
+++ app/src/main/java/com/albertojr/proyectoandroidsuperpoderes/ui/navigation/Screens.kt
@@ -7,14 +7,14 @@ sealed class Screens(val route: String) {
     object HeroesDetailScreen : Screens(SCREEN2_ROUTE_TEMPLATE){
         const val ARG_HEROE_ID = "heroeId"
         fun createRouteWithArgs(heroeId: Long): String{ //TODO check
-        //   return SCREEN2_ROUTE_TO_FORMAT.format(heroeId) //Returns HeroesDetailScreen/heroeId
-            return "HeroesDetailScreen/$heroeId"
+           return SCREEN2_ROUTE_TO_FORMAT.format(heroeId) //Returns HeroesDetailScreen/heroeId
+           // return "HeroesDetailScreen/$heroeId"
         }
     }
     companion object{
         private const val SCREEN1_BASE_ROUTE = "HeroesListScreen"
         private const val SCREEN2_BASE_ROUTE = "HeroesDetailScreen"
         private const val SCREEN2_ROUTE_TEMPLATE = "$SCREEN2_BASE_ROUTE/{$ARG_HEROE_ID}"
-        private const val SCREEN2_ROUTE_TO_FORMAT = "$SCREEN2_BASE_ROUTE/$ARG_HEROE_ID"
+        private const val SCREEN2_ROUTE_TO_FORMAT = "$SCREEN2_BASE_ROUTE/%s"
     }
 }
